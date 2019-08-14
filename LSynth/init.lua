c12f8c62e532a8469d5d1b7fbd28268f24eabb1c
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-08-14 13:59:17 +0300

    Remove comments about sfxr

--- LSynth/init.lua
+++ LSynth/init.lua
@@ -40,8 +40,7 @@ Call before any other method !
 
 Arguments:
 - channels (number/nil): (unsigned int) The number of channels to have.
-- sampleRate (number/nil): (unsigned int) The sample rate to operate on, note that SFXR mode only supports 44100 and 22050.
-	By default it's 44100 on PC, and 22050 on mobile.
+- sampleRate (number/nil): (unsigned int) The sample rate to operate on, by default it's 44100 on PC, and 22050 on mobile.
 - bufferLength (number/nil): (unsigned float) The length of the buffer in seconds, by defaults it's 1/60.
 - piecesCount (number/nil): (unsigned int) The number of pieces to divide the buffer into, affects responsivity, by default it's 4.
 ]]
--- LSynth/thread.lua
+++ LSynth/thread.lua
@@ -2,8 +2,7 @@
 
 --[[
 - channels (number): (unsigned int)
-- sampleRate (number): (unsigned int) The sample rate to operate on, note that SFXR mode only supports 44100 and 22050.
-	By default it's 44100 on PC, and 22050 on mobile.
+- sampleRate (number): (unsigned int) The sample rate to operate on, by default it's 44100 on PC, and 22050 on mobile.
 - bufferLength (number): (unsigned float) The length of the buffer in seconds, by defaults it's 1/60.
 - piecesCount (number): (unsigned int) The number of pieces to divide the buffer into, affects responsivity, by default it's 4.
 - inChannel (userdata): (love channel) The input channel, recieves data from the main thread.
@@ -14,3 +13,4 @@ local channels, sampleRate, bufferLength, piecesCount, inChannel = ...
 require("love.timer")
 require("love.sound")
 require("love.audio")
+
