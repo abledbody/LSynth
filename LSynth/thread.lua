0f12a9193af95d683a9dc3fd851ef60e60078c3d
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-08-14 14:05:05 +0300

    Nothing remarkable

--- LSynth/thread.lua
+++ LSynth/thread.lua
@@ -1,7 +1,9 @@
 --The chip thread
 
+--TODO: Make sure the parameters explaination is correct after testing them.
+
 --[[
-- channels (number): (unsigned int)
+- channels (number): (unsigned int) The number of channels to have.
 - sampleRate (number): (unsigned int) The sample rate to operate on, by default it's 44100 on PC, and 22050 on mobile.
 - bufferLength (number): (unsigned float) The length of the buffer in seconds, by defaults it's 1/60.
 - piecesCount (number): (unsigned int) The number of pieces to divide the buffer into, affects responsivity, by default it's 4.
@@ -13,4 +15,6 @@ local channels, sampleRate, bufferLength, piecesCount, inChannel = ...
 require("love.timer")
 require("love.sound")
 require("love.audio")
+
+local channelStore = {} --Stores each channel parameters.
 
