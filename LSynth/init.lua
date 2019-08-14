55b4033d4ac3e71e711005d447403fc9eb97979c
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-08-14 13:54:52 +0300

    Add channels count argument

--- LSynth/init.lua
+++ LSynth/init.lua
@@ -9,7 +9,9 @@ Optional love modules:
 love.system (Used to know if running on mobile)
 
 Love modules used by threads:
-
+love.timer (Used to calculate the sleeping time)
+love.sound (Used for creating sounddatas)
+love.audio (Used for playing the generated sounddatas)
 ]]
 
 local path = (...) --The library require path.
@@ -23,6 +25,7 @@ local LSynth = {}
 LSynth.initialized = false --Has the LSynth chip been initialized ?
 LSynth.isMobile = false --Is the chip running on a mobile device ?
 
+LSynth.channels = 4 --Default channels count.
 LSynth.sampleRate = 44100 --Default sample rate (samples per second), 22050 on mobile (if detected).
 LSynth.bufferLength = 1/60 --Default buffer length (in seconds).
 LSynth.piecesCount = 4 --Default count of buffer pieces (affects responsivity).
@@ -36,12 +39,13 @@ LSynth.outChannel = nil --The channel which sends data into the LSynth thread.
 Call before any other method !
 
 Arguments:
-- sampleRate (number/nil): (int) The sample rate to operate on, note that SFXR mode only supports 44100 and 22050.
+- channels (number/nil): (unsigned int) The number of channels to have.
+- sampleRate (number/nil): (unsigned int) The sample rate to operate on, note that SFXR mode only supports 44100 and 22050.
 	By default it's 44100 on PC, and 22050 on mobile.
-- bufferLength (number/nil): (float) The length of the buffer in seconds, by defaults it's 1/60.
-- piecesCount (number/nil): (int) The number of pieces to divide the buffer into, affects responsivity, by default it's 4.
+- bufferLength (number/nil): (unsigned float) The length of the buffer in seconds, by defaults it's 1/60.
+- piecesCount (number/nil): (unsigned int) The number of pieces to divide the buffer into, affects responsivity, by default it's 4.
 ]]
-function LSynth:initialize(sampleRate, bufferLength, piecesCount)
+function LSynth:initialize(channels, sampleRate, bufferLength, piecesCount)
 	if self.initialized then error("Already initialized!") end
 
 	--Check if running on mobile, if love.system is available.
@@ -50,6 +54,8 @@ function LSynth:initialize(sampleRate, bufferLength, piecesCount)
 	--Lower the sample rate if running on mobile.
 	if self.isMobile then LSynth.sampleRate = 22050 end
 
+	--Override the channels count.
+	if channels then self.channels = channels end
 	--Override the sample rate.
 	if sampleRate then self.sampleRate = sampleRate end
 	--Override the buffer length.
@@ -63,7 +69,7 @@ function LSynth:initialize(sampleRate, bufferLength, piecesCount)
 	--Load the thread
 	self.thread = love.thread.newThread(dir.."/thread.lua")
 	--Start the thread
-	self.thread:start(self.sampleRate, self.bufferLength, self.piecesCount, self.outChannel)
+	self.thread:start(self.channels ,self.sampleRate, self.bufferLength, self.piecesCount, self.outChannel)
 
 	self.initialized = true
 end
