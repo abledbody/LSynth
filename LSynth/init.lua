97f1d15bf1ec3142a659e3420651dece91633e57
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-08-14 12:50:11 +0300

    Initial setup

---
+++ LSynth/init.lua
@@ -0,0 +1,11 @@
+--LSynth by RamiLego4Game & LIKO-12's Organization
+--The portable fantasy audio chip made for LIKO-12 which could be used in any LÖVE project.
+
+--[[
+Required love modules:
+
+]]
+
+local LSynth = {}
+
+return LSynth
---
+++ main.lua
@@ -0,0 +1,14 @@
+--LSynth Demo
+local LSynth = require("LSynth")
+
+function love.load()
+
+end
+
+function love.draw()
+
+end
+
+function love.update(dt)
+
+end
