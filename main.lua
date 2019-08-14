b22de7eeed8b90432e62d1a69795b6a3eb8400c9
Rami Sabbagh (Windows) <ramilego4game@gmail.com>
2019-08-14 13:02:59 +0300

    Add thread error handler

--- main.lua
+++ main.lua
@@ -10,5 +10,9 @@ function love.draw()
 end
 
 function love.update(dt)
+	LSynth:update(dt)
+end
 
+function love.threaderror(thread, errorstr)
+	error("Thread error ("..tostring(thread).."): "..tostring(errorstr))
 end
