bb34925848a094dbc4091a9a0cd5ca4e990b34a2
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-12-11 18:11:09 +0200

    Comments improvement

--- LSynth/thread.lua
+++ LSynth/thread.lua
@@ -8,8 +8,8 @@
 - channels (number): (unsigned int) The number of channels to have, by default it's 4.
 - sampleRate (number): (unsigned int) The sample rate to operate on, by default it's 44100 on PC, and 22050 on mobile.
 - bitDepth (number): (unsigned int) The bitdepth of the generated samples, by default it's 8 (for fantasy reasons).
-- bufferLength (number): (unsigned float) The length of the buffer in seconds, by default it's 1/60.
-- piecesCount (number): (unsigned int) The number of pieces to divide the buffer into, affects responsivity, by default it's 4.
+- bufferLength (number): (unsigned float) The length of the buffer in seconds, affects responsivity and pops/clicks, by default it's 1/15.
+- piecesCount (number): (unsigned int) The number of pieces to divide the buffer into, affects pops/clicks, by default it's 4.
 - inChannel (userdata): (love channel) The input channel, recieves data from the main thread.
 ]]
 local path, dir, channels, sampleRate, bitDepth, bufferLength, piecesCount, inChannel = ...
@@ -28,6 +28,8 @@ local floor, min, max = math.floor, math.min, math.max
 --== Constants ==--
 local pieceSamplesCount = floor((bufferLength*sampleRate)/piecesCount) --The length of buffer pieces in samples.
 local bufferSamplesCount = pieceSamplesCount*piecesCount --The length of the buffer in samples.
+--Please note that the bufferSamplesCount is not the same of (bufferLength*sampleRate),
+--The buffersSamplesCount makes sure that the buffer is perfect multiplication of the pieceSamplesCount.
 
 --== Variables ==--
 local channelStore = {} --Stores each channel parameters.
