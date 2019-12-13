bef339ad4c2506b0e17af5e2cb0a380a649ffc0f
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-12-14 00:00:46 +0200

    New testing code

--- main.lua
+++ main.lua
@@ -14,6 +14,17 @@ function love.update(dt)
 
 	if love.keyboard.isDown("space") and not LSynth.initialized then
 		LSynth:initialize(2)
+
+		LSynth:setPanning(0, 0)
+		LSynth:setAmplitude(0, 1)
+		LSynth:setFrequency(0, 200)
+		LSynth:setWaveform(0,0)
+		LSynth:enable(0)
+		for i=0, 200 do
+			LSynth:wait(0,0.125)
+			LSynth:setFrequency(0, 250 + i*10)
+			LSynth:setPanning(0, i%3-1)
+		end
 	elseif LSynth.initialized and not love.keyboard.isDown("space") then
 		love.event.quit()
 	end
